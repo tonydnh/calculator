444036a2380877d3585b4118b57fb6f5cbc6b514
Tony Dinh <tonydinh442@gmail.com>
2024-06-03 15:23:58 -0500

    Add functionality to all number buttons and operations

--- app.js
+++ app.js
@@ -1,6 +1,79 @@
-let numOne;
-let numTwo;
-let operator;
+let numOne = "0";
+let numTwo = "";
+let operator = "";
+let shouldClear = false;
+let operatorPressed = false;
+const display = document.querySelector(".display-screen");
+const numberKeys = document.querySelectorAll(".number");
+const operatorKeys = document.querySelectorAll(".operator");
+const equalsKey = document.querySelector(".equals-key");
+const clearKey = document.querySelector(".clear-key");
+
+numberKeys.forEach(key => key.addEventListener("click", () => appendNumber(key.textContent)));
+operatorKeys.forEach(key => key.addEventListener("click", () => saveOperator(key.textContent)));
+
+clearKey.addEventListener("click", () => {
+    display.textContent = "0";
+    operatorPressed = false;
+    clear();
+});
+
+equalsKey.addEventListener("click", () =>  {
+    if (numTwo !== "") {
+        console.log(`${numOne} ${operator} ${numTwo}`);
+        display.textContent = operate(parseFloat(numOne), parseFloat(numTwo), operator);
+        operatorPressed = false;
+        shouldClear = true;
+        clear();
+        // Save last computed answer as first operand
+        numOne = display.textContent;
+    }
+});
+
+function clear() {
+    numOne = "";
+    numTwo = "";
+    operator = "";
+}
+
+function appendNumber(text) {
+    if (display.textContent === "0" || shouldClear) display.textContent = "";
+    if (shouldClear) shouldClear = false;
+    // Get rid of initial zero when calculator starts
+    if (numOne === "0" && !operatorPressed) numOne = "";
+
+    if (operatorPressed) {
+        numTwo += text;
+    } else {
+        numOne += text;
+    }
+
+    display.textContent += text;
+}
+
+function saveOperator(symbol) {
+    shouldClear = true;
+    operatorPressed = true;
+
+    if (numTwo !== "") {
+        numOne = operate(parseFloat(numOne), parseFloat(numTwo), operator);
+        numTwo = "";
+        display.textContent = numOne;
+    }
+
+    operator = getOperator(symbol);
+}
+
+function getOperator(symbol) {
+    if (symbol === "÷") {
+        return "/";
+    } else if (symbol === "×") {
+        return "*";
+    } else if (symbol === "−") {
+        return "-";
+    }
+    return "+";
+}
 
 function operate(a, b, operator) {
     if (operator === "+") {
